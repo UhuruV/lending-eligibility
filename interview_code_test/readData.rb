5edc73d3b1d360199f6bb44bdfd64f3baf8c8e15
UhuruV <uhuruvalary@gmail.com>
2022-01-17 13:58:21 +0300

    looping through grouped array

--- interview_code_test/readData.rb
+++ interview_code_test/readData.rb
@@ -6,15 +6,15 @@ require 'pp'
 # customerData = CSV.read('transaction_data_1.csv',headers: true)
 
 # customer_data = JSON.pretty_generate(CSV.open('transaction_data_1.csv', headers:true).map do |row|
-#     modifier = {}
+#     customerData = {}
 #     row.each do |k,v|
-#         if k =~ /modifier(.)_(.*)$/
-#             (modifier[$1] ||= {})[$2]= v
+#         if k =~ /customerData(.)_(.*)$/
+#             (customerData[$1] ||= {})[$2]= v
 #         end
 #     end
 #     {
 #         id:row['id'],
-#             modifier: modifier.sort_by { |k,v|k}.map{|k,v|v}
+#             customer_id: customerData.sort_by { |k,v|k}.map{|k,v|v}
 #     }
 # end
 # )
@@ -28,8 +28,16 @@ hashed_data = JSON.parse(customer_data)
  
 grouped_data = hashed_data.group_by { |k| k['customer_id'] }
 
-#Count the length of each grouped array
-# using Enumerable frequency policy to get the frequency of occurance of the arrays
+#looping through the grouped data
+grouped_data.each do |customer|
+    puts customer
+end 
+
+#Get consecutive dates
+# Have a sliding window to keep track of the maximum consecutive days and n number (the legnth of the array)
+max_consecutive_dates = 0
+start = 0
+max_days = 0
 
 
-puts grouped_data
+
